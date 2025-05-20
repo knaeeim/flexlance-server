9b075e662b69661262fbd8932fec89cfdca88f5d
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2025-05-20 08:55:00 +0530

    DataBase Setup done and make a api for user registration

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+
+const uri =
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.plgxbak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const postedData = client.db("flexlance").collection("postedData");
+        const userCollection = client.db("flexlance").collection("users");
+
+        app.get('/', (req, res) => {
+            res.send('Flexlance server is running');
+        })
+
+        // POST API for user data
+        app.get('/users', async (req, res) => {
+            const user = await userCollection.find().toArray()
+            res.send(user);
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
