29a3872d683e495f8cfba6db98f89e4ca84c276a
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2025-05-21 12:15:21 +0530

    Created Delete API

--- index.js
+++ index.js
@@ -87,6 +87,14 @@ async function run() {
             res.send(result);
         })
 
+        // delete a specific data
+        app.delete('/deleteData/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id : new ObjectId(id) };
+            const result = await postedDataCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log(
